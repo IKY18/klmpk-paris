ece605459d2f265863a6efc0827d95d9e279c8d0
VirgilWeol <virgilweol25@gmail.com>
2023-06-14 09:38:09 +0800

    commit pertemuan 8

---
+++ tes.js
@@ -0,0 +1,14 @@
+export let nama = "John Doe";
+
+export let john = {
+    fullName: "John Doe",
+    age: 33,
+    isMarried: true,
+};
+
+const greetings = () => {
+    return `Hello World`
+};
+
+export default greetings;
+export let angka = [1, 2, 3, 4, 5];
